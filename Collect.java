a08665bfcef6503ea515cfccca34bb043b1af123
lis1304 <isl1304@yandex.ru>
2017-12-22 10:36:04 +0400

    коллекции

--- Collect.java
+++ Collect.java
@@ -1,11 +1,10 @@
 package collect;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 
 abstract class Collect {
+    /*
    static void print(List list){
         Iterator iter = list.iterator();
         while (iter.hasNext()){
@@ -13,6 +12,13 @@ abstract class Collect {
         }
     }
     static void print(Set set){
+        Iterator iter = set.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        }
+    }
+    */
+    static void print(Collection set){
         Iterator iter = set.iterator();
         while (iter.hasNext()){
             System.out.println(iter.next());
